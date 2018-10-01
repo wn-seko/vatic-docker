9117304ee3d26942357d8c3efb0c8a53e25fe0b7
wn-seko <wataru_nakaseko@albert2005.co.jp>
2018-10-01 11:45:18 +0900

    change directory

--- ascripts/myphp.php
+++ ascripts/myphp.php
@@ -12,7 +12,8 @@
             ini_set('display_startup_errors',1);
             error_reporting(-1);
             $slug = $_POST['video'];
-            $result = exec("cd ../..; turkic dump ${slug} --xml -o /root/vatic/data/output/${slug}.xml 2>&1; mysqldump -u root --all-databases > data/db.mysql");
+            make_dir("/root/vatic/data/output/${slug}");
+            $result = exec("cd ../..; turkic dump ${slug} --xml -o /root/vatic/data/output/${slug}/${slug}.xml 2>&1; mysqldump -u root --all-databases > data/db.mysql");
             echo $result;
             exit();
         case 'doVisualize':
@@ -21,8 +22,8 @@
             ini_set('display_startup_errors',1);
             error_reporting(-1);
             $slug = $_POST['video'];
-            make_dir("/root/vatic/data/output/${slug}");
-            $result = exec("cd ../..; turkic visualize ${slug} /root/vatic/data/output/${slug} --merge --renumber --labels 2>&1;");
+            make_dir("/root/vatic/data/output/${slug}/annotated_imgs");
+            $result = exec("cd ../..; turkic visualize ${slug} /root/vatic/data/output/${slug}/annotated_imgs --merge --renumber --labels 2>&1;");
             echo $result;
             exit();
     }
